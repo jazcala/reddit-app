c886a7131f201b8987462aa767c2f53551232c24
jazcala <jazcalabrese@gmail.com>
2025-04-03 09:15:06 -0300

    Fixing styles for posts

--- src/components/posts/post/Post.tsx
+++ src/components/posts/post/Post.tsx
@@ -6,27 +6,25 @@ import styles from "./Post.module.scss";
 export default function Post({ post }: { post: postProps }) {
   return (
     <div className={styles.post}>
-      <div>
-        {(post.url.includes("jpg") ||
-          post.url.includes("png") ||
-          post.url.includes("jpeg")) && (
+      {post.url.includes("jpg") ||
+        post.url.includes("png") ||
+        (post.url.includes("jpeg") && (
           <img src={post.url} alt={`Image of ${post.title}`} />
-        )}
-        <h3>{post.title}</h3>
-        <p>{post.description}</p>
-        <div className={styles.bottom}>
-          <div className={styles.author}>
-            By <a href="#">{post.author}</a>
-          </div>
-          <div className={styles.score}>
-            <BsHandThumbsDown />
-            {post.score}
-            <BsHandThumbsUp />
-          </div>
-          <div className={styles.comments}>
-            <FaRegComment />
-            {post.num_comments}
-          </div>
+        ))}
+      <h3>{post.title}</h3>
+      <p>{post.description}</p>
+      <div className={styles.bottom}>
+        <div className={styles.author}>
+          By <a href="#">{post.author}</a>
+        </div>
+        <div className={styles.score}>
+          <BsHandThumbsDown />
+          {post.score}
+          <BsHandThumbsUp />
+        </div>
+        <div className={styles.comments}>
+          <FaRegComment />
+          {post.num_comments}
         </div>
       </div>
     </div>
---
+++ src/features/posts/postsSlice.ts
@@ -0,0 +1,73 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchPostById, fetchPosts, fetchSearchResults } from "../../api/api";
+import { postSliceInitialStateProps } from "../../types/types";
+
+const initialState: postSliceInitialStateProps = {
+  posts: [],
+  status: 'idle',
+  error: "",
+  query: "",
+};
+
+export const postSlice = createSlice({
+  name: "posts",
+  initialState,
+
+  reducers: {
+    setScore: (state, action) => {
+      const { postId, newScore } = action.payload;
+      const post = state.posts.find((p) => p.id === postId);
+      if (post) {
+        post.score = newScore;
+      }
+    },
+    setQuery: (state, action) => {
+      state.query = action.payload;
+    },
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPosts.pending, (state) => {
+        state.status = "loading";
+        state.error = "";
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.error = "";
+        state.posts = action.payload;
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error?.message || "Fetch posts failed";
+      })
+      .addCase(fetchPostById.fulfilled, (state, action) => {
+        const existingPost = state.posts.find(
+          (post) => post.id === action.payload.id
+        );
+        if (!existingPost) {
+          state.posts.push({ ...action.payload, voteState: "neutral" });
+        }
+      })
+      .addCase(fetchSearchResults.pending, (state) => {
+        state.status = 'loading';
+        state.error = "";
+      })
+      .addCase(fetchSearchResults.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.error = "";
+        state.posts = action.payload;
+      })
+      .addCase(fetchSearchResults.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error?.message || "Fetch searched posts failed";
+      });
+  },
+});
+
+export const selectPostById = (state: postSliceInitialStateProps, postId: string) =>
+  state.posts.find((post) => post.id === postId);
+
+export const { setScore,
+  setQuery } = postSlice.actions;
+export default postSlice.reducer;
