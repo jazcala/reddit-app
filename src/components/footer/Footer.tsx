bfb44a4315a09c4847c56c54ba63682d9b422dc3
jazcala <jazcalabrese@gmail.com>
2025-04-04 19:16:47 -0300

    adding target attribute

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,8 +3,15 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <p>
-        2025 - By <a href="https://github.com/jazcala">@jazcala</a> -
-        <a href="https://github.com/jazcala/reddit-app"> sourceCode</a>
+        2025 - By{" "}
+        <a href="https://github.com/jazcala" target="_blank">
+          @jazcala
+        </a>{" "}
+        -
+        <a href="https://github.com/jazcala/reddit-app" target="_blank">
+          {" "}
+          sourceCode
+        </a>
       </p>
     </footer>
   );
